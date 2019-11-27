df9c62be80114ce3913de5af13284c070507258b
adam <adamthalib09@gmail.com>
2019-11-27 16:04:44 +0800

    test

---
+++ src/main/java/com/mitrais/smartclinic/controller/controller.java
@@ -0,0 +1,10 @@
+package com.mitrais.smartclinic.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class controller {
+    @RequestMapping("/home")
+    public String home() { return "index"; }
+}
