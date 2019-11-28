d2e115d05e8de9601ad5aedb64aa687505d4297c
andhika-tanaka <andhika.tanaka@gmail.com>
2019-11-28 14:08:17 +0800

    #2 Updating feature : Removing Medical Record Number (Auto)

--- src/main/java/com/mitrais/smartclinic/controller/PatientController.java
+++ src/main/java/com/mitrais/smartclinic/controller/PatientController.java
@@ -12,38 +12,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/patients")
 public class PatientController {
 
-    private final
-    PatientRepository patientRepository;
+    private final PatientRepository patientRepository;
 
     public PatientController(PatientRepository patientRepository) {
         this.patientRepository = patientRepository;
     }
 
-    private String medicalRecordNumber(Integer id) {
-        String mrn = "A";
-        if (id < 10) {
-            mrn = mrn + "000" + id.toString();
-        } else if (id<100){
-            mrn = mrn + "00" + id.toString();
-        } else if(id<1000){
-            mrn = mrn + "0" + id.toString();
-        } else{
-            mrn = mrn + id.toString();
-        }
-        return mrn;
-    }
-
     @GetMapping("/add")
     public String showPatientForm(Model model) {
-        Integer id = patientRepository.lastID();
-        if(id == null){
-            id = 1;
-        } else {
-            id = id++;
-        }
-        String mrn = medicalRecordNumber(id);
         Patient patient = new Patient();
-        patient.setMedicalRecordNumber(mrn);
         model.addAttribute(patient);
         return "patients/form-add";
     }
--- src/main/java/com/mitrais/smartclinic/repository/PatientRepository.java
+++ src/main/java/com/mitrais/smartclinic/repository/PatientRepository.java
@@ -2,14 +2,8 @@ package com.mitrais.smartclinic.repository;
 
 import com.mitrais.smartclinic.model.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PatientRepository extends JpaRepository<Patient, Long> {
-
-    @Query("SELECT MAX(p.id) FROM Patient p " +
-            "ORDER BY p.id " +
-            "DESC")
-    Integer lastID();
 }
