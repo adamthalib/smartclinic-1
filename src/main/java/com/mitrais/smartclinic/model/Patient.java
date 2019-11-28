ba60a79d5409edc3080c1a477cb52b7cddff69e2
andhika-tanaka <andhika.tanaka@gmail.com>
2019-11-28 17:16:56 +0800

    #2 Updating feature : Form Validation

--- src/main/java/com/mitrais/smartclinic/model/Patient.java
+++ src/main/java/com/mitrais/smartclinic/model/Patient.java
@@ -3,6 +3,8 @@ package com.mitrais.smartclinic.model;
 import lombok.Data;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 @Data
@@ -11,21 +13,36 @@ public class Patient {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotNull
+    @Size(min = 2, max = 60)
     private String firstName;
 
+    @Size(max = 60)
     private String lastName;
 
     private String email;
 
+    @NotNull
     private String dob;
 
+    @NotNull
     private String idNumber;
 
-    private String medicalRecordNumber;
-
+    @NotNull(message = "Please Choose 1!")
     private String gender;
 
-    private String address;
+    @NotNull
+    private String street;
+
+    @NotNull
+    @Size(min = 5 , max = 5)
+    private String postCode;
+
+    @NotNull
+    private String city;
+
+    @NotNull
+    private String province;
 
     private String phone1;
 
