a8348427e1abd2fee5f4497f64517ced46a5f7f2
andhika-tanaka <andhika.tanaka@gmail.com>
2019-11-28 09:55:29 +0800

    #2 Adding feature : Create Patient

---
+++ src/main/java/com/mitrais/smartclinic/controller/PatientController.java
@@ -0,0 +1,34 @@
+package com.mitrais.smartclinic.controller;
+
+import com.mitrais.smartclinic.model.Patient;
+import com.mitrais.smartclinic.repository.PatientRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/patients")
+public class PatientController {
+
+    private final
+    PatientRepository patientRepository;
+
+    public PatientController(PatientRepository patientRepository) {
+        this.patientRepository = patientRepository;
+    }
+
+    @GetMapping("/add")
+    public String showPatientForm(Model model) {
+        Patient patient = new Patient();
+        model.addAttribute(patient);
+        return "patients/form-add";
+    }
+
+    @PostMapping("/add")
+    public String savePatient(Patient patient) {
+        patientRepository.save(patient);
+        return "redirect:/";
+    }
+}
