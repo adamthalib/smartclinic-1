85d2d9f4503cb127c359ac7a7097fc5d69eddc19
adamthalib <adamthalib09@gmail.com>
2019-11-29 15:19:23 +0800

    Adding Display Patient in Table Adding Delete Patient

--- src/main/java/com/mitrais/smartclinic/controller/PatientController.java
+++ src/main/java/com/mitrais/smartclinic/controller/PatientController.java
@@ -2,20 +2,31 @@ package com.mitrais.smartclinic.controller;
 
 import com.mitrais.smartclinic.model.Patient;
 import com.mitrais.smartclinic.repository.PatientRepository;
+import org.dom4j.rule.Mode;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/patients")
 public class PatientController {
 
-    private final PatientRepository patientRepository;
+    @Autowired
+    PatientRepository patientRepository;
 
-    public PatientController(PatientRepository patientRepository) {
-        this.patientRepository = patientRepository;
+
+    @GetMapping
+    public String patient(Model model){
+        List<Patient> patients = patientRepository.findAll();
+        model.addAttribute("patients", patients);
+        return "patients/patient";
     }
 
     @GetMapping("/add")
@@ -28,6 +39,11 @@ public class PatientController {
     @PostMapping("/add")
     public String savePatient(Patient patient) {
         patientRepository.save(patient);
-        return "redirect:/";
+        return "redirect:/patients";
+    }
+    @GetMapping(value = "/delete/{id}")
+    public String deletePatient(@PathVariable("id") Long id){
+        patientRepository.deleteById(id);
+        return "redirect:/patients";
     }
 }
--- src/main/java/com/mitrais/smartclinic/model/Patient.java
+++ src/main/java/com/mitrais/smartclinic/model/Patient.java
@@ -30,4 +30,92 @@ public class Patient {
     private String phone1;
 
     private String phone2;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getIdNumber() {
+        return idNumber;
+    }
+
+    public void setIdNumber(String idNumber) {
+        this.idNumber = idNumber;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getMedicalRecordNumber() {
+        return medicalRecordNumber;
+    }
+
+    public void setMedicalRecordNumber(String medicalRecordNumber) {
+        this.medicalRecordNumber = medicalRecordNumber;
+    }
+
+    public String getPhone1() {
+        return phone1;
+    }
+
+    public String getPhone2() {
+        return phone2;
+    }
+
+    public void setPhone1(String phone1) {
+        this.phone1 = phone1;
+    }
+
+    public void setPhone2(String phone2) {
+        this.phone2 = phone2;
+    }
 }
